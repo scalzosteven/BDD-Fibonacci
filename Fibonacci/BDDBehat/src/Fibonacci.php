751391a6d1005af9f3207efe1cb42e164fa1b338
steven <stevemscalzo@grupojuinsa.es>
2020-02-17 12:45:52 +0100

    GREEN:  Get Fibonacci Sequence Number for index 3

--- Fibonacci/BDDBehat/src/Fibonacci.php
+++ Fibonacci/BDDBehat/src/Fibonacci.php
@@ -5,10 +5,19 @@ class Fibonacci
 {
     public function getValueByIndex($index){
 
-        if($index == 1){
-            return 0;
-        } else if($index == 2){
-            return 1;
+//        if($index == 1){
+//            return 0;
+//        } else if($index == 2){
+//            return 1;
+//        }
+        $first = 0;
+        $second = 1;
+        for($i = 0; $i < ($index - 1); $i++){
+            $third = $first + $second;
+            $first = $second;
+            $second = $third;
         }
+
+        return $first;
     }
 }
