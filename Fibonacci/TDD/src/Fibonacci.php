a88e699734da12bce394fcb4e6ae0a996f9c3b7c
steven <stevemscalzo@grupojuinsa.es>
2020-02-17 10:23:59 +0100

    GREEN: If_Index_Is_Two_Value_Is_One

--- Fibonacci/TDD/src/Fibonacci.php
+++ Fibonacci/TDD/src/Fibonacci.php
@@ -10,6 +10,8 @@ class Fibonacci
         $indexToReturn = null;
         if($index == 1){
             $indexToReturn = 0;
+        } else if($index == 2){
+            $indexToReturn = 1;
         }
 
         return $indexToReturn;
